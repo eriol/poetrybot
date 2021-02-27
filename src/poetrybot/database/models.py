8c8ad31c573d80b1ddb983ef588573c0e8ed7e54
Daniele Tricoli <eriol@mornie.org>
2021-02-27 03:49:27 +0100

    Add Poet model

--- src/poetrybot/database/models.py
+++ src/poetrybot/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Boolean
+from sqlalchemy import Column, Integer, Boolean, String
 
 from . import Base
 
@@ -16,3 +16,15 @@ class User(Base):
 
     def __repr__(self):
         return f"User(id={self.id}, is_staff={self.is_staff})"
+
+
+class Poet(Base):
+    """A poet."""
+
+    __tablename__ = "poets"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(length=150, collation="NOCASE"), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f"Poet(id={self.id}, name={self.name})"
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,9 @@
 import pytest
 
+from sqlalchemy.exc import IntegrityError
+
 from poetrybot.database import store
-from poetrybot.database.models import User
+from poetrybot.database.models import User, Poet
 
 
 @pytest.fixture
@@ -16,7 +18,7 @@ def test_users_empty(db):
     db.session.remove()
 
 
-def test_users_creation_with_default_values(db):
+def test_users_creation_with_default_parameters(db):
     """Test creation of an user without parameters."""
     s = db.session()
     s.add(User())
@@ -39,5 +41,45 @@ def test_users_creation_staff(db):
     u = s.query(User).one()
     assert u.id == 1
     assert u.is_staff is True
+
+    db.session.remove()
+
+def test_poets_empty(db):
+    s = db.session()
+    assert s.query(Poet).all() == []
+    db.session.remove()
+
+def test_poets_creation_no_parameters(db):
+    """Test poet creation without parameters."""
+
+    s = db.session()
+    s.add(Poet())
+
+    with pytest.raises(IntegrityError):
+        s.commit()
+
+    db.session.remove()
+
+def test_poets_creation(db):
+    """Test poet creation."""
+
+    poet_name = "Eugenio Montale"
+
+    s = db.session()
+    s.add(Poet(name=poet_name))
+    s.commit()
+
+    poet = s.query(Poet).one()
+    assert poet.id == 1
+    assert poet.name == poet_name
+
+    s.add(Poet(name=poet_name))
+    with pytest.raises(IntegrityError):
+        s.commit()
+    db.session.rollback()
+
+    s.add(Poet(name=poet_name.upper()))
+    with pytest.raises(IntegrityError):
+        s.commit()
 
     db.session.remove()
