e9c4505681b851d2dbc172f55ff125b021b12c53
Daniele Tricoli <eriol@mornie.org>
2021-03-07 20:11:49 +0100

    Connect to the database

--- src/poetrybot/main.py
+++ src/poetrybot/main.py
@@ -1,15 +1,21 @@
 import logging
 
+from poetrybot.database import store
+from poetrybot.config import Config
+
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(name)s - %(levelname)s - %(message)s",
 )
 
+config = Config.from_environ()
 log = logging.getLogger(__name__)
 
 
 def main():
     log.debug("Starting poetrybot")
+
+    store.connect(config.DATABASE_URL)
 
 
 if __name__ == "__main__":
