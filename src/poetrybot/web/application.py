0b85c576b6230b425700e3eb04562dbd69b5e80c
Daniele Tricoli <eriol@mornie.org>
2021-03-08 00:50:36 +0100

    Add an endpoint to create poets

--- src/poetrybot/web/application.py
+++ src/poetrybot/web/application.py
@@ -20,10 +20,10 @@ def error(status_code, message=None):
     if message:
         payload["message"] = message
 
-    r = jsonify(payload)
-    r.status_code = status_code
+    response = jsonify(payload)
+    response.status_code = status_code
 
-    return r
+    return response
 
 
 @app.route("/poets", methods=["GET"])
@@ -33,4 +33,27 @@ def get_poets():
     poets = s.query(Poet).all()
     store.session.remove()
 
-    return jsonify(poets)
+    return jsonify([poet.to_dict() for poet in poets])
+
+
+@app.route("/poets", methods=["POST"])
+def create_poet():
+    data = request.get_json() or {}
+
+    if "name" not in data:
+        return error(400, "name field is required")
+    if data["name"] == "":
+        return error(400, "name field can't be empty")
+
+    s = store.session()
+
+    if s.query(Poet).filter(Poet.name == data["name"]).first():
+        return error(400, "this poet is already present")
+
+    poet = Poet(name=data["name"])
+    s.add(poet)
+    s.commit()
+
+    response = jsonify(poet.to_dict())
+    response.status_code = 201
+    return response
