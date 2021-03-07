4cad18fc3a652dc75ec32e9be41dc83a5d338bf4
Daniele Tricoli <eriol@mornie.org>
2021-03-07 20:07:23 +0100

    Convert main to an entrypoint script

--- src/poetrybot/main.py
+++ src/poetrybot/main.py
@@ -4,7 +4,13 @@ logging.basicConfig(
     level=logging.DEBUG,
     format="%(name)s - %(levelname)s - %(message)s",
 )
+
 log = logging.getLogger(__name__)
 
-if __name__ == "__main__":
+
+def main():
     log.debug("Starting poetrybot")
+
+
+if __name__ == "__main__":
+    main()
