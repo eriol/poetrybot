f9ebc11167b2ec5422e0fe7851d12313396ff211
Daniele Tricoli <eriol@mornie.org>
2021-03-16 01:47:49 +0100

    Use database session context manager

--- src/poetrybot/telegram/commands/quote.py
+++ src/poetrybot/telegram/commands/quote.py
@@ -7,8 +7,7 @@ from poetrybot.database.api import get_a_random_poem
 
 def quote(update: Update, context: CallbackContext) -> None:
     """Get a poem."""
-    s = store.session()
-    poem = get_a_random_poem(s)
-    store.session.remove()
+    with store.get_session() as s:
+        poem = get_a_random_poem(s)
 
     update.message.reply_text(poem.verses)
