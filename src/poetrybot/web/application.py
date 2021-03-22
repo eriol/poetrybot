4e6cae38c393984d8ec5a1df5d9790d5e7e7c970
Daniele Tricoli <eriol@mornie.org>
2021-03-22 02:24:46 +0100

    Check for Authorization header before every request

--- src/poetrybot/web/application.py
+++ src/poetrybot/web/application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 from poetrybot.config import Config
 from poetrybot.database import store
@@ -8,11 +8,24 @@ from .poems import bp as poems_bp
 
 
 def create_app(config=None):
+    """Create a Flask app with specified configuration.
+
+    If config is None the configuration is made using environment variables.
+    """
     if config is None:
         config = Config.from_environ()
     store.connect(config.DATABASE_URL)
 
     app = Flask(__name__)
+
+    @app.before_request
+    def check_auth_token():
+        """Check every request for the Authorization header."""
+        auth_token = request.headers.get("Authorization")
+
+        if auth_token != config.AUTH_TOKEN:
+            return "", 401
+
     app.register_blueprint(poets_bp, url_prefix="/poets")
     app.register_blueprint(poems_bp, url_prefix="/poems")
 
--- tests/web/conftest.py
+++ tests/web/conftest.py
@@ -3,14 +3,21 @@ import pytest
 from poetrybot.config import Config
 from poetrybot.web.application import create_app
 
+TEST_AUTH_TOKEN = "a super secret token"
+
 
 @pytest.fixture()
 def client():
     """Get a test client."""
     config = Config.from_environ(
-        {"DATABASE_URL": "sqlite:///:memory:", "TELEGRAM_TOKEN": ""}
+        {
+            "AUTH_TOKEN": TEST_AUTH_TOKEN,
+            "DATABASE_URL": "sqlite:///:memory:",
+            "TELEGRAM_TOKEN": "",
+        }
     )
     app = create_app(config)
 
     with app.test_client() as client:
+        client.environ_base["HTTP_AUTHORIZATION"] = TEST_AUTH_TOKEN
         yield client
