98cd31b732c35a863ceb3ad415133e42e40b0bde
Daniele Tricoli <eriol@mornie.org>
2021-03-30 20:07:53 +0200

    Fix __repr__ of User class

--- src/poetrybot/database/models.py
+++ src/poetrybot/database/models.py
@@ -13,7 +13,7 @@ class User(Base):
     name = Column(String(length=150), nullable=False)
 
     def __repr__(self):
-        return f"User(id={self.id}, is_staff={self.is_staff})"
+        return f"User(id={self.id}, name={self.name})"
 
 
 class Poet(Base):
