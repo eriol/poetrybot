2a40634fbd271a3671d8a03d6e86f5791dc30f7b
Daniele Tricoli <eriol@mornie.org>
2021-03-30 23:51:16 +0200

    Reply without quoting the invoked command using markdown

--- src/poetrybot/telegram/commands/quote.py
+++ src/poetrybot/telegram/commands/quote.py
@@ -1,8 +1,12 @@
-from telegram import Update
+import logging
+
+from telegram import Update, ParseMode
 from telegram.ext import CallbackContext
 
 from poetrybot.database import store
 from poetrybot.database.api import get_a_random_poem
+
+logger = logging.getLogger(__name__)
 
 
 def quote(update: Update, context: CallbackContext) -> None:
@@ -10,5 +14,8 @@ def quote(update: Update, context: CallbackContext) -> None:
     with store.get_session() as s:
         poem = get_a_random_poem(s)
 
-    reply = poem.verses if poem else "No quote found!"
-    update.message.reply_text(reply)
+        reply = f"{poem.verses}\n\n_{poem.author.name}_" if poem else "No quote found!"
+
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text=reply, parse_mode=ParseMode.MARKDOWN
+    )
